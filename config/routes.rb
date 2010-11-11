40b10fa86c7c5660eea489404123873bdeb463d9
One Art <spam8@oneart.com>
2010-11-11 16:10:03 -0500

    Map root url to folio index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Gallery::Application.routes.draw do
+  resources :folios
+
   resources :artworks
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Gallery::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "folios#index"
 
   # See how all your routes lay out with "rake routes"
 
